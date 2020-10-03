76525ef17c2b5b4bff3844dd1eee7763a9ad8172
titarakshay <titarakshay@gmail.com>
2020-10-03 06:37:32 +0530

    name-changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
       <div className="d-flex justify-content-between align-items-color p-3 px-3">
         <img src={logo} alt="logo" style={{ width: "5rem" }} />
         <div>
-          <h1>Product Deatils</h1>
+          <h1>Product Store</h1>
         </div>
         <div>
           <h2>E-Mart</h2>
